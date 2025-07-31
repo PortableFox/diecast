d04102fbaeb513aed3a0e074f6139185dd265796
PortableFox <181051199+PortableFox@users.noreply.github.com>
2025-07-30 17:16:23 -0700

    Change how sdice nodes are handled

--- src/wbtk_diecast/dice.py
+++ src/wbtk_diecast/dice.py
@@ -448,32 +448,23 @@ class StatefulDice:
     '''
     Constructor
     -----------
-    :param nodes (list[dice]): list of dice representing the dice rolled for each state
-    :param links (dict{int:{int:int}}): dictionary that maps node ids to a map from node dice roll values to linked node ids.
-                                        node ids are determined by the index of their dice in the nodes list argument.
+    :param name (str): label for stateful dice
     :param verbose (bool): whether or not to print verbose dice state details (primarily for debugging purposes)
     '''
-    def __init__(self, nodes = [], links = [], name = None, verbose = False):
+    def __init__(self, name = None, verbose = False):
         self.name = name if name else f"sdice<custom>"
         self.verbose = verbose
-        self.node = self.TERMINAL
-        self.start_node = self.TERMINAL
+        self.node = StatefulDice.TERMINAL
+        self.start_node = StatefulDice.TERMINAL
         self.node_list = {
-            self.TERMINAL: {
+            StatefulDice.TERMINAL: {
+                'name': 'Terminal Node',
                 'dice':  Dice.uniform([None]),
                 'edges': {
-                    None: self.TERMINAL
+                    None: StatefulDice.TERMINAL
                 }
             }
         }
-        if len(nodes) > 0:
-            self.node = 0
-            self.start_node = 0
-            for node in nodes:
-                self.add_node(node)
-        for link in links:
-            for roll in links[link]:
-                self.set_node_link(link, roll, links[link][roll])
     
     def __call__(self):
         return self.roll()
@@ -492,12 +483,15 @@ class StatefulDice:
         return f'StatefulDice'
     
     def __str__(self):
-        fmt_nid = lambda node_id: "TERMINAL_NODE" if node_id == self.TERMINAL else node_id
-        fmt_nid_info = lambda node_id, data: "n/a" if node_id == self.TERMINAL else str(data)
+        fmt_nid = lambda node_id: "StatefulDice.TERMINAL" if node_id == StatefulDice.TERMINAL else node_id
+        fmt_nid_info = lambda node_id, data: "n/a" if node_id == StatefulDice.TERMINAL else str(data)
         
         res = f'{self.name} stateful dice {{\n'
         for node in self.node_list:
             res += f'\tnode id: {fmt_nid(node)}\n'
+            res += f'\t\t name: {self.node_list[node]["name"]}\n'
+            if node == StatefulDice.TERMINAL:
+                continue
             res += f'\t\t dice: {fmt_nid_info(node, self.node_list[node]["dice"].__repr__())}\n'
             res += f'\t\tlinks: {fmt_nid_info(node, {(val, fmt_nid(nid)) for val, nid in self.node_list[node]["edges"].items()})}\n'
         res += f'\tstart node: {fmt_nid(self.start_node)}\n'
@@ -505,39 +499,15 @@ class StatefulDice:
         res += f'}}'
         return res
     
-    def add_node(self, dice):
-        node_id = len(self.node_list)-1
-        self.node_list[node_id] = {
-            'dice': dice,
-            'edges': {side: node_id for side in dice.values}
-        }
-        if node_id == 0:
-            self.start_node = 0
-            self.node = 0
-    
     def current_dice(self):
         return self.node_list[self.node]['dice']
     
-    def set_node_link(self, node_id, link_val, link_id):
+    def loop_node_link(self, node_id, link_val):
         if node_id not in self.node_list:
             raise ValueError(f"node {node_id} not in node list")
-        if link_id not in self.node_list:
-            raise ValueError(f"link node {link_id} not in node list")
         if link_val not in self.node_list[node_id]['dice']:
             raise ValueError(f"{link_val} is not a valid side for node {node_id}")
-        self.node_list[node_id]['edges'][link_val] = link_id
-    
-    def set_node_links(self, node_id, links):
-        for val, link in links:
-            self.set_node_link(node_id, val, link)
-    
-    def set_node_terminator(self, node_id, value):
-        if node_id in self.node_list and value in self.node_list[node_id]['dice'].values:
-            self.node_list[node_id][value] = self.self.TERMINAL
-    
-    def set_start_node(self, node_id):
-        if node_id in self.node_list:
-            self.start_node = node_id
+        self.node_list[node_id]['edges'][link_val] = node_id
     
     def reset(self):
         if self.verbose:
@@ -558,8 +528,35 @@ class StatefulDice:
                 rolls.append(self.roll())
         return rolls
     
+    def set_node(self, node_id, dice, name = None, default_link = None):
+        self.node_list[node_id] = {
+            'name': name if name else dice.name,
+            'dice': dice,
+            'edges': {side: default_link if default_link else self.TERMINAL for side in dice.values}
+        }
+     
+    def set_node_link(self, node_id, link_val, link_id):
+        if node_id not in self.node_list:
+            raise ValueError(f"node {node_id} not in node list")
+        if link_id not in self.node_list:
+            raise ValueError(f"link node {link_id} not in node list")
+        if link_val not in self.node_list[node_id]['dice']:
+            raise ValueError(f"{link_val} is not a valid side for node {node_id}")
+        self.node_list[node_id]['edges'][link_val] = link_id
+
+    def set_start_node(self, node_id):
+        if node_id in self.node_list:
+            self.start_node = node_id
+    
+    def terminate_node_link(self, node_id, link_val):
+        if node_id not in self.node_list:
+            raise ValueError(f"node {node_id} not in node list")
+        if link_val not in self.node_list[node_id]['dice']:
+            raise ValueError(f"{link_val} is not a valid side for node {node_id}")
+        self.node_list[node_id]['edges'][link_val] = StatefulDice.TERMINAL
+        
     def terminated(self):
-        return self.node_list[self.node]['dice'] == self.node_list[self.TERMINAL]['dice']
+        return self.node_list[self.node]['dice'] == self.node_list[StatefulDice.TERMINAL]['dice']
     
     def update_state(self, value):
         try:
